a8e673c5d6289723c2f71e32be4b47e05c0944d6
wakaflocka17 <fra.congiu17@gmail.com>
2025-04-14 20:24:45 +0200

    I've modified the model configs parameter

--- src/model_configs.py
+++ src/model_configs.py
@@ -2,22 +2,22 @@
 MODEL_CONFIGS = {
     'bart_base': {
         'model_name': 'facebook/bart-base',
-        'epochs': 2,
-        'batch_size': 8,
+        'epochs': 5,
+        'batch_size': 16,
         'learning_rate': 2e-5,
         'repo': 'models/bart-base',  # Added repo parameter
     },
     'bert_base_uncased': {
         'model_name': 'bert-base-uncased',
-        'epochs': 2,
-        'batch_size': 8,
+        'epochs': 4,
+        'batch_size': 16,
         'learning_rate': 2e-5,
         'repo': 'models/bert-base-uncased',
     },
     'gpt_neo_2_7b': {
         'model_name': 'EleutherAI/gpt-neo-2.7B',
-        'epochs': 2,
-        'batch_size': 8,  # Smaller batch size for large model
+        'epochs': 3,
+        'batch_size': 4,  # Smaller batch size for large model
         'learning_rate': 1e-5,
         'gradient_accumulation_steps': 4,
         'repo': 'models/gpt-neo-2.7B',  # Added repo parameter

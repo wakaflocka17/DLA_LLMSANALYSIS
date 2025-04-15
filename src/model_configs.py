e384d644b35ddcc9e414047dbe3df2cd05d3c258
wakaflocka17 <fra.congiu17@gmail.com>
2025-04-15 15:26:45 +0200

    I've modified the configurations because they were too expensive

--- src/model_configs.py
+++ src/model_configs.py
@@ -2,8 +2,8 @@ MODEL_CONFIGS = {
     'bart_base': {
         'model_name': 'facebook/bart-base',
         'epochs': 5,
-        'train_batch_size': 16,
-        'eval_batch_size': 32,
+        'train_batch_size': 8,
+        'eval_batch_size': 4,
         'learning_rate': 2e-5,
         'repo_pretrained': 'models/pretrained/bart-base',
         'repo_finetuned': 'models/finetuned/bart-base-imdb',
@@ -11,8 +11,8 @@ MODEL_CONFIGS = {
     'bert_base_uncased': {
         'model_name': 'google-bert/bert-base-uncased',
         'epochs': 5,
-        'train_batch_size': 16,
-        'eval_batch_size': 32,
+        'train_batch_size': 8,
+        'eval_batch_size': 4,
         'learning_rate': 2e-5,
         'repo_pretrained': 'models/pretrained/bert-base-uncased',
         'repo_finetuned': 'models/finetuned/bert-base-uncased-imdb',
@@ -20,8 +20,8 @@ MODEL_CONFIGS = {
     'gpt_neo_2_7b': {
         'model_name': 'EleutherAI/gpt-neo-2.7B',
         'epochs': 3,
-        'train_batch_size': 4,
-        'eval_batch_size': 4,
+        'train_batch_size': 2,
+        'eval_batch_size': 2,
         'learning_rate': 1e-5,
         'gradient_accumulation_steps': 4,
         'repo_pretrained': 'models/pretrained/gpt-neo-2.7B',
@@ -30,7 +30,7 @@ MODEL_CONFIGS = {
     'ensemble_majority_voting': {
         'model_names': ['facebook/bart-base', 'bert-base-uncased', 'EleutherAI/gpt-neo-2.7B'],
         'train_batch_size': 8,
-        'eval_batch_size': 16,
+        'eval_batch_size': 4,
         'epochs': 3,
         'repo': 'models/ensemble/majority-voting-imdb'
     }
